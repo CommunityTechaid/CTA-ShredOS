1ff6dc5741ac396cccbe4668c0131ddbbc5077ce
Akhil Muraleedharan <akhil.hardys@gmail.com>
2024-05-16 15:25:40 +0100

    Modified the end of the nwipe_launcher script to terminate after the wipe and show the end of process message. Also changed ./post.sh to suppress errors if no post scripts are found so that user is not confused.

--- board/shredos/fsoverlay/usr/bin/post.sh
+++ board/shredos/fsoverlay/usr/bin/post.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-for f in /usr/bin/scripts/post_*.sh; do
-	    # if this execution fails, then stop the `for`:
-	echo "Executing $f"
-	if ! bash "$f"; then
-		break;
-		echo "There was an error when executing script $f. Ignoring rest of the scripts"
-	fi
-done
+if [ "$(ls -A /usr/bin/scripts/post_*.sh 2>/dev/null)" ]; then
+	for f in /usr/bin/scripts/post_*.sh; do
+		# if this execution fails, then stop the `for`:
+		echo "Executing $f"
+		if ! bash "$f"; then
+			break;
+			echo "There was an error when executing script $f. Ignoring rest of the scripts"
+		fi
+	done
+else:
+	echo "No post hook scripts to run"
+fi
